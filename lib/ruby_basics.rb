8c7fe45314d3a03661d3ffff721bf8cab049712c
Tominiyi Ajayi <tominiyiajayi@gmail.com>
2018-12-22 09:31:47 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,12 +1,24 @@
+<<<<<<< HEAD
 def division (num1, num2)
+=======
+ def division(num1, num2)
+>>>>>>> fb7f280f01bd09d9083b4a28fc4882235f169593
     return num1 / num2
 end
   
 def assign_variable(value)
+<<<<<<< HEAD
     return value
 end
   
 def argue(str)
+=======
+    return value = "Tomi" 
+end
+  
+def argue(str)
+    str = ""
+>>>>>>> fb7f280f01bd09d9083b4a28fc4882235f169593
     return str
 end
   
@@ -14,6 +26,7 @@ def greeting(greeting, name)
     return greeting + name
 end
   
+<<<<<<< HEAD
 def return_a_value
     return "Nice"
 end
@@ -24,4 +37,16 @@ end
   
 def pizza_party (default = "cheese")
     return default
+=======
+def return_a_value(Nice)
+    return 
+end
+  
+def last_evaluated_value(expert)
+    return 
+end
+  
+def pizza_party (default = "cheese")
+    return 
+>>>>>>> fb7f280f01bd09d9083b4a28fc4882235f169593
 end
